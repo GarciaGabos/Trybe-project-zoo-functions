eacf74a5784c0b831a5d67c641eb39210955a149
Gabriel Garcia <philiasmaximus@gmail.com>
2022-06-07 19:45:24 -0300

    Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -8,7 +8,17 @@ function isManager(id) {
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  try {
+    const wantedEmployees = employees.filter((manager) => manager.managers.includes(managerId));
+    const wantedEmplyeesOrganized = wantedEmployees.map((arrayOfEmployees) =>
+      `${arrayOfEmployees.firstName} ${arrayOfEmployees.lastName}`);
+    return wantedEmplyeesOrganized;
+  } catch (error) {
+    return error.message;
+  }
 }
 
 module.exports = { isManager, getRelatedEmployees };
