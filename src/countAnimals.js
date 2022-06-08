368ef07707ec0d3a8df7bd8f1f0e96fa04be87be
Gabriel Garcia <philiasmaximus@gmail.com>
2022-06-08 03:48:09 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,39 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
+const countAllAnimals = () => {
+  const allAnimalsList = {};
+  species.forEach((eachSpecies) => {
+    const nameOfeachSpecies = eachSpecies.name;
+    const quantitySpecies = eachSpecies.residents.length;
+    allAnimalsList[nameOfeachSpecies] = quantitySpecies;
+  });
+  return allAnimalsList;
+};
+
+const countOneAnimalUnissex = (animal) => {
+  const wantedSpecies = species.find((eachSpecies) => eachSpecies.name === animal.specie);
+  return wantedSpecies.residents.length;
+};
+
+const countOneAnimalSex = (animal) => {
+  const wantedSpecies = species.find((eachSpecies) => eachSpecies.name === animal.specie);
+  const wantedSpeciesGender = wantedSpecies.residents.filter((eachResident) => eachResident.sex
+  === animal.sex);
+  return wantedSpeciesGender.length;
+};
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    return countAllAnimals();
+  }
+  if (animal.sex === undefined) {
+    return countOneAnimalUnissex(animal);
+  }
+  if (animal.sex !== undefined) {
+    return countOneAnimalSex(animal);
+  }
 }
 
 module.exports = countAnimals;
