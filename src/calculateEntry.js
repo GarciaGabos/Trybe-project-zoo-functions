6fdb7bd9faa4ec14eedf88da9accd06c237943d3
Gabriel Garcia <philiasmaximus@gmail.com>
2022-06-08 14:40:12 -0300

    Requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,40 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const entrantsByAge = {
+    child: entrants.filter(({ age }) => age < 18).length,
+    adult: entrants.filter(({ age }) => age >= 18 && age < 50).length,
+    senior: entrants.filter(({ age }) => age >= 50).length };
+  return entrantsByAge;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const quantityEntrants = countEntrants(entrants);
+  const sumOfentries = (quantityEntrants.child * 20.99) + (quantityEntrants.adult * 49.99)
+  + (quantityEntrants.senior * 24.99);
+  return sumOfentries;
 }
 
 module.exports = { calculateEntry, countEntrants };
+
+// child: são pessoas menores de 18 anos;
+// adult: são pessoas com idade maior ou igual a 18 anos e menor que 50 anos;
+// senior: são pessoas com idade maior ou igual a 50 anos.
+
+// Considerando a boa prática de dividir o código em partes menores, o arquivo terá duas funções, chamadas de countEntrants e calculateEntry.
+
+// As duas funções recebem um array no seguinte formato:
+
+// const entrants = [
+//   { name: 'Lara Carvalho', age: 5 },
+//   { name: 'Frederico Moreira', age: 5 },
+//   { name: 'Pedro Henrique Carvalho', age: 5 },
+//   { name: 'Maria Costa', age: 18 },
+//   { name: 'Núbia Souza', age: 18 },
+//   { name: 'Carlos Nogueira', age: 50 },
+// ];
+
+// console.log(countEntrants(entrants));
