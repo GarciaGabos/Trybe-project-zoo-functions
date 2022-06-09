35ca40afaaac4015c5037299ec5b32846bcf8e90
Gabriel Garcia <philiasmaximus@gmail.com>
2022-06-09 02:57:59 -0300

    Requisito 1

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,27 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { species, employees } = data;
+
+function buildResult(worker) {
+  const speciesFiltered = species.filter((s) => worker.responsibleFor.includes(s.id));
+  return {
+    id: worker.id,
+    fullName: `${worker.firstName} ${worker.lastName}`,
+    species: speciesFiltered.map((s) => s.name),
+    locations: speciesFiltered.map((s) => s.location),
+  };
+}
+function getEmployeesCoverage(employee) {
+  if (employee === undefined) {
+    return employees.map((worker) => buildResult(worker));
+  }
+  const employeeWanted = employees.find((e) => e.id === employee.id
+  || e.firstName === employee.name || e.lastName === employee.name);
+
+  if (employeeWanted === undefined) {
+    throw new Error('Informações inválidas');
+  }
+  return buildResult(employeeWanted);
 }
 
 module.exports = getEmployeesCoverage;
